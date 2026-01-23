a43ec6afeb5c9f526bc0cdaebc1381734d9927f6
Simonas Zilevicius GP Office <s.zilevicius@gmail.com>
2026-01-23 14:04:51 +0100

    Process update

---
+++ Projects/Casual coding/casual_coding.js
@@ -0,0 +1,8 @@
+let a = 5;
+console.log("The value of a is:", a);
+
+a = 6;
+console.log("The new value of a is:", a);
+
+let name = prompt("What is your name?");
+console.log("Hello, " + name + "!");
